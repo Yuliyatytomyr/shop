b7e3475fa35b8c1c50ec3593e0e1dbaa68758df3
Yuliyatytomyr <jtitomir@gmail.com>
2020-03-20 21:37:26 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './main.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import {applyMiddleware, createStore} from "redux";
+import {createBrowserHistory} from "history";
+import thunk from "redux-thunk";
+import {routerMiddleware, ConnectedRouter} from "connected-react-router";
+import {composeWithDevTools} from "redux-devtools-extension";
+import {Provider} from "react-redux";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import createRootReducer from "reducers";
+import routes from "./node_modules/routes";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+const history = createBrowserHistory();
+const middlewares = [thunk, routerMiddleware(history)];
+const store = createStore(
+    createRootReducer(history),
+    composeWithDevTools(applyMiddleware(...middlewares)),
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            {routes}
+        </ConnectedRouter>
+        <ToastContainer
+            position="top-right"
+            autoClose={2000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnVisibilityChange
+            draggable
+            pauseOnHover
+        />
+    </Provider>,
+    document.getElementById('root')
+);
